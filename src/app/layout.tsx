e18b7fbc34cbfa2e6d035f0415095df6568de0d1
Wasif1016 <wasifdevelopsite@gmail.com>
2026-04-13 22:06:16 -0700

    feat: initialize root layout with custom fonts, global providers, and marketing tracking scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17539389650"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17539389650');
+            `,
+          }}
+        />
         <script
           dangerouslySetInnerHTML={{
             __html: `
